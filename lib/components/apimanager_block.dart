3320bb5a941040ae94d2008098972cea9dcdcdc9
Aritra <aritrabiswas9788@gmail.com>
2024-06-13 01:08:14 +0530

    feat: Created separate component to handle multiple API keys

---
+++ lib/components/apimanager_block.dart
@@ -0,0 +1,168 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+import 'galaxytextfield.dart';
+
+class ApiManagerBlock extends StatefulWidget {
+  ApiManagerBlock(
+      {super.key,
+      required this.description,
+      required this.title,
+      required this.urlDesc1,
+      required this.urlDesc2,
+      this.urlLauncher1,
+      this.urlLauncher2,
+      this.keyController,
+      required this.screenHeight,
+      required this.screenWidth});
+
+  String title;
+  String description;
+  String urlDesc1;
+  VoidCallback? urlLauncher1;
+  String urlDesc2;
+  VoidCallback? urlLauncher2;
+  TextEditingController? keyController;
+  double screenHeight;
+  double screenWidth;
+
+  @override
+  State<ApiManagerBlock> createState() => _ApiManagerBlockState();
+}
+
+class _ApiManagerBlockState extends State<ApiManagerBlock> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 20.0),
+        child: ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Container(
+                height: widget.screenHeight * 0.54,
+                width: widget.screenWidth * 0.9,
+                child: Stack(children: [
+                  BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 3, sigmaY: 3),
+                    child: Container(),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        border:
+                            Border.all(color: Colors.white.withOpacity(0.1)),
+                        borderRadius: BorderRadius.circular(5),
+                        gradient: LinearGradient(colors: [
+                          Colors.white.withOpacity(0.1),
+                          Colors.white.withOpacity(0.1)
+                        ])),
+                  ),
+                  Center(
+                      child: Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.max,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                widget.title,
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 35,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: widget.screenWidth * 0.2),
+                                child: Divider(
+                                  color: Colors.white.withOpacity(0.8),
+                                  thickness: 1.2,
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 10.0,
+                              ),
+                              Text(
+                                widget.description,
+                                textAlign: TextAlign.center,
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.w300,
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 10.0,
+                              ),
+                              Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.black.withOpacity(0.4),
+                                  border: Border.all(
+                                      color: Colors.white.withOpacity(0.1)),
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(16.0),
+                                  child: Row(
+                                    mainAxisSize: MainAxisSize.max,
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
+                                    children: [
+                                      Material(
+                                        color: Colors.transparent,
+                                        child: InkWell(
+                                          borderRadius:
+                                              BorderRadius.circular(10),
+                                          splashColor:
+                                              Colors.blue.withOpacity(0.3),
+                                          onTap: widget.urlLauncher1,
+                                          child: Text(
+                                            widget.urlDesc1,
+                                            textAlign: TextAlign.center,
+                                            style: const TextStyle(
+                                              color: Colors.blue,
+                                              fontSize: 25,
+                                              fontWeight: FontWeight.w300,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                      Material(
+                                        color: Colors.transparent,
+                                        child: InkWell(
+                                          borderRadius:
+                                              BorderRadius.circular(10),
+                                          splashColor:
+                                              Colors.blue.withOpacity(0.3),
+                                          onTap: widget.urlLauncher2,
+                                          child: Text(
+                                            widget.urlDesc2,
+                                            textAlign: TextAlign.center,
+                                            style: const TextStyle(
+                                              color: Colors.blue,
+                                              fontSize: 25,
+                                              fontWeight: FontWeight.w300,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 10.0,
+                              ),
+                              GalaxyTextField(
+                                  hintText:
+                                      "eg, ZaCELgL.0imfnc8mVLWwsAawjYr4Rx",
+                                  labelText: "PLACES API-KEY",
+                                  iconData: Icons.code,
+                                  textInputType: TextInputType.text,
+                                  controller: widget.keyController,
+                                  isPassword: true)
+                            ],
+                          )))
+                ]))));
+  }
+}
--- lib/tabs/apikey_tab.dart
+++ lib/tabs/apikey_tab.dart
@@ -2,8 +2,9 @@ import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:super_liquid_galaxy_controller/components/galaxytextfield.dart';
+import 'package:super_liquid_galaxy_controller/components/apimanager_block.dart';
 import 'package:url_launcher/url_launcher.dart';
+
 //ignore_for_file: prefer_const_constructors
 //ignore_for_file: prefer_const_literals
 
@@ -29,136 +30,23 @@ class _ApiKeyTabState extends State<ApiKeyTab> {
             vertical: screenHeight * 0.05, horizontal: screenHeight * 0.15),
         child: SingleChildScrollView(
           child: Column(mainAxisSize: MainAxisSize.max, children: [
-            Padding(
-                padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 20.0),
-                child: ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: Container(
-                        height: screenHeight * 0.54,
-                        width: screenWidth * 0.9,
-                        child: Stack(children: [
-                          BackdropFilter(
-                            filter: ImageFilter.blur(sigmaX: 3, sigmaY: 3),
-                            child: Container(),
-                          ),
-                          Container(
-                            decoration: BoxDecoration(
-                                border: Border.all(
-                                    color: Colors.white.withOpacity(0.1)),
-                                borderRadius: BorderRadius.circular(5),
-                                gradient: LinearGradient(colors: [
-                                  Colors.white.withOpacity(0.1),
-                                  Colors.white.withOpacity(0.1)
-                                ])),
-                          ),
-                          Center(
-                              child: Padding(
-                                  padding: EdgeInsets.all(15.0),
-                                  child: Column(
-                                    mainAxisSize: MainAxisSize.max,
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      Text(
-                                        "Geo-Apify Places API",
-                                        style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 35,
-                                          fontWeight: FontWeight.w400,
-                                        ),
-                                      ),
-                                      Padding(
-                                        padding: EdgeInsets.symmetric(
-                                            horizontal: screenWidth * 0.2),
-                                        child: Divider(
-                                          color: Colors.white.withOpacity(0.8),
-                                          thickness: 1.2,
-                                        ),
-                                      ),
-                                      SizedBox(
-                                        height: 10.0,
-                                      ),
-                                      Text(
-                                        "This API is used by the App to dynamically fetch a huge list of recommended of POIs to explore.",
-                                        textAlign: TextAlign.center,
-                                        style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 25,
-                                          fontWeight: FontWeight.w300,
-                                        ),
-                                      ),
-                                      SizedBox(
-                                        height: 10.0,
-                                      ),
-                                      Container(
-                                        decoration: BoxDecoration(
-                                          color: Colors.black.withOpacity(0.4),
-                                          border: Border.all(
-                                              color: Colors.white
-                                                  .withOpacity(0.1)),
-                                          borderRadius:
-                                              BorderRadius.circular(10),
-                                        ),
-                                        child: Padding(
-                                          padding: const EdgeInsets.all(16.0),
-                                          child: Row(
-                                            mainAxisSize: MainAxisSize.max,
-                                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                            children: [
-                                              Material(
-                                                color: Colors.transparent,
-                                                child: InkWell(
-                                                  borderRadius: BorderRadius.circular(10),
-                                                  splashColor: Colors.blue.withOpacity(0.3),
-                                                  onTap: (){
-                                                    launchUrlFromLink(Uri.parse("https://apidocs.geoapify.com/playground/places/"));
-                                                  },
-                                                  child: Text(
-                                                    "GeoApify | Places API Playground ",
-                                                    textAlign: TextAlign.center,
-                                                    style: TextStyle(
-                                                      color: Colors.blue,
-                                                      fontSize: 25,
-                                                      fontWeight: FontWeight.w300,
-                                                    ),
-                                                  ),
-                                                ),
-                                              ),
-                                              Material(
-                                                color: Colors.transparent,
-                                                child: InkWell(
-                                                  borderRadius: BorderRadius.circular(10),
-                                                  splashColor: Colors.blue.withOpacity(0.3),
-                                                  onTap: (){
-                                                    launchUrlFromLink(Uri.parse("https://myprojects.geoapify.com/projects"));
-                                                  },
-                                                  child: Text(
-                                                    "GeoApify | Create API KEY ",
-                                                    textAlign: TextAlign.center,
-                                                    style: TextStyle(
-                                                      color: Colors.blue,
-                                                      fontSize: 25,
-                                                      fontWeight: FontWeight.w300,
-                                                    ),
-                                                  ),
-                                                ),
-                                              ),
-                                            ],
-                                          ),
-                                        ),
-                                      ),
-                                      SizedBox(
-                                        height: 10.0,
-                                      ),
-                                      GalaxyTextField(
-                                          hintText:
-                                              "eg, ZaCELgL.0imfnc8mVLWwsAawjYr4Rx",
-                                          labelText: "PLACES API-KEY",
-                                          iconData: Icons.code,
-                                          textInputType: TextInputType.text,
-                                          isPassword: true)
-                                    ],
-                                  )))
-                        ])))),
+            ApiManagerBlock(
+              description:
+                  "This API is used by the App to dynamically fetch a huge list of recommended of POIs to explore.",
+              title: "Geo-Apify Places API",
+              urlDesc1: "GeoApify | Places API Playground ",
+              urlDesc2: "GeoApify | Create API KEY ",
+              screenHeight: screenHeight,
+              screenWidth: screenWidth,
+              urlLauncher1:() {
+                launchUrlFromLink(
+                    Uri.parse(
+                        "https://apidocs.geoapify.com/playground/places/"));
+              },
+              urlLauncher2:(){ launchUrlFromLink(
+                  Uri.parse("https://myprojects.geoapify.com/projects"));},
+              keyController: TextEditingController(),
+            ),
           ]),
         ),
       ),
@@ -172,10 +60,8 @@ class _ApiKeyTabState extends State<ApiKeyTab> {
         print("error + $url");
         throw Exception('Could not launch $url');
       }
-    }
-    catch(e){
+    } catch (e) {
       print("$url:  $e");
     }
-
   }
 }
